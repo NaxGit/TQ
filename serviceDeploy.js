78289b7add846d7d4095301490682a900fc5cadf
NaxGit <145691347+NaxGit@users.noreply.github.com>
2024-05-01 15:32:52 +0300

    Update serviceDeploy.js

--- serviceDeploy.js
+++ serviceDeploy.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator)
 {
-    navigator.serviceWorker.register('./serviceWorker.js', { scope: '/' })
+    navigator.serviceWorker.register('./serviceWorker.js', { scope: '/TQ/' })
     .then(registration =>
     {console.log('Service Worker registered');})
     .catch(error =>
