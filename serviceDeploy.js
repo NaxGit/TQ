31a90ef538e617defbc577c327a4ba20925985b6
NaxGit <145691347+NaxGit@users.noreply.github.com>
2024-05-01 15:27:27 +0300

    Add files via upload

---
+++ serviceDeploy.js
@@ -0,0 +1,8 @@
+if ('serviceWorker' in navigator)
+{
+    navigator.serviceWorker.register('./serviceWorker.js', { scope: '/' })
+    .then(registration =>
+    {console.log('Service Worker registered');})
+    .catch(error =>
+    {console.error('Service Worker registration failed:', error);});
+}
